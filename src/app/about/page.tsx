54336973320058142d9726e043b25844252ae2e6
theborakompanioni <theborakompanioni+github@gmail.com>
2023-12-31 14:56:54 +0100

    build(deps): install react-daisyui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button } from 'react-daisyui'
+
+export default function About() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      It works!
+      <Button>a</Button>
+    </main>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/daisyui/dist/**/*.js', 
+    'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {
@@ -15,6 +17,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 }
 export default config
